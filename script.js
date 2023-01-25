2c911dd515c5e647986e391aa0e52bf0f66df48d
Fabian Winkler <winkler.fabian22@gmx.at>
2023-01-25 23:44:25 +0100

    created jumpto links

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 AOS.init();
 
-
+$(document).ready(function() {
+    $("#service_link").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({
+            scrollTop: $("#service").offset().top - 70
+        }, 2000);
+    });
+    $("#about_link").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({
+            scrollTop: $("#about").offset().top - 70
+        }, 2000);
+    });
+    $("#references_link").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({
+            scrollTop: $("#references").offset().top - 70
+        }, 2000);
+    });
+    $("#home_link").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({
+            scrollTop: $("#home").offset().top - 70
+        }, 2000);
+    });
+});
 
 $(".service_col").hover(
     function(){
